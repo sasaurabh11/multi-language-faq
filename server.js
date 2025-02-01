aa1bf2e4b252c8136f6a230a3de489407329d193
Saurabh <sesaurabh8052@gmail.com>
2025-02-01 14:43:52 +0530

    feat: initialised project and complete database connection

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import "dotenv/config";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import connectDB from "./db/index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port http://localhost:${PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Error in connecting to DB", error);
+})
+
+export default app;
